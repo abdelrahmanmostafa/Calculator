a9e25e71121768aaae327e51414d8cc2c9ad5e33
abdelrahmanmostafa <abdelrahmanmostafa@users.noreply.github.com>
2015-09-15 00:00:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "main.hpp"
 using namespace std;
 
-  int main (int argc, char ** argv)
+int main (int argc, char ** argv)
 {
     startCalc();
 	return 0;
@@ -13,50 +13,48 @@ void startCalc()
 {
     calculator  calc;
     int operation=0;
-	while(1)
-	{
+    while(1)
+    {
     cout<<endl;
-	cout<<"********Simple  Caclulator********"<<endl;
+    cout<<"********Simple  Caclulator********"<<endl;
     cout<<"select an operation"<<endl;
-    cout<<"1:addition"<<endl;
-    cout<<"2:substraction"<<endl;
-    cout<<"3:muliplication"<<endl;
-    cout<<"4:division"<<endl;
+    cout<<"1:Addition"<<endl;
+    cout<<"2:Substraction"<<endl;
+    cout<<"3:Muliplication"<<endl;
+    cout<<"4:Division"<<endl;
     cout<<"5:Modulo"<<endl;
-    cout<<"6:exit"<<endl;
+    cout<<"6:Exit"<<endl;
     cout<<"--------------------------------------------------------------------"<<endl;
     cout<<"Enter your choice :";
-	cin>>operation;
+    cin>>operation;
     switch(operation)
+     {
+	case 1:
+	 {
+         enterInputs();
+	 calc.sum(num1,num2);
+	 break;
+	 }
+	case 2:
 	{
-		case 1:
-		{
-        enterInputs();
-		calc.sum(num1,num2);
-		break;
-		}
-		case 2:
-		{
         enterInputs();
         calc.sub(num1,num2);
         break;
-		}
-		case 3:
-		{
+	}
+	case 3:
+	{
         enterInputs();
         calc.mul(num1,num2);
         break;
-		
-		}
-		case 4:
-		{
+	}
+	case 4:
+	{
         enterInputs();
         calc.div(num1,num2);
         break;
-		
-		}
-		case 5:
-		{
+	}
+	case 5:
+	{
         enterInputs();
         calc.mod(num1,num2);
         break;
@@ -69,8 +67,7 @@ void startCalc()
         default:
         {
         cout<<"\nunvalid choice,try again"<<endl;
-		}
-
+	}
 	}
  }
 }
