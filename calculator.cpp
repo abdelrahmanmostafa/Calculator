eb2258b951b906c299aac8d22e6d6531f27256df
abdelrahmanmostafa <abdelrahmanmostafa@users.noreply.github.com>
2015-09-15 03:10:25 +0200

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,22 +5,22 @@ using namespace std;
     void calculator::sum (const double x,const double y)
       { 
        cout<<"\n The Result is "<<x + y<<endl;
-	  }
+       }
     void calculator::sub (const double x,const double y)
       {
         cout<<"\n The Result is "<<x - y<<endl;
-	  }
+      }
     void calculator::mul (const double x,const double y)
       {
             cout<<"\n The result is "<<x * y<<endl;
-	   }
+      }
     void calculator::div (const double x,const double y)
       {
           cout<<"\n The Result is "<<x / y<<endl;
-	  }
+      }
     void calculator::mod (const int x,const int y)
       {
            cout<<"\n The Result is "<<x % y<<endl;
-	  }
+       }
 	
 
